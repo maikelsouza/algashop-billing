99dd784161f46b8cfa2206025524f555f1996471
RE045908 <re045908@qintess.com>
2025-09-22 14:52:52 -0300

    SMS level 2 - 16.16 - Application Service with CQRS for Invoice Generation

---
+++ src/main/java/com/algaworks/algashop/billing/application/invoce/management/PayerData.java
@@ -0,0 +1,23 @@
+package com.algaworks.algashop.billing.application.invoce.management;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PayerData {
+
+    private String fullName;
+
+    private String document;
+
+    private String email;
+
+    private String phone;
+
+    private AddressData address;
+}
